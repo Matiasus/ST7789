1e5fe250bc819889152e1778b4d4161947a9c14f
Marián <Matiasus@users.noreply.github.com>
2023-12-04 11:45:11 +0100

    Test init sequence

--- src/st7789.h
+++ src/st7789.h
@@ -1,7 +1,7 @@
-/** 
- * --------------------------------------------------------------------------------------------+ 
+/**
+ * --------------------------------------------------------------------------------------------+
  * @name        ST7789 1.69" LCD Driver
- * --------------------------------------------------------------------------------------------+ 
+ * --------------------------------------------------------------------------------------------+
  *              Copyright (C) 2023 Marian Hrinko.
  *              Written by Marian Hrinko (mato.hrinko@gmail.com)
  *
@@ -15,10 +15,8 @@
  * --------------------------------------------------------------------------------------------+
  * @descr       Version 1.0 -> applicable for 1 display
  * --------------------------------------------------------------------------------------------+
- * @inspir      
+ * @inspir
  */
-
-#include <avr/pgmspace.h>
 
 #ifndef __ST7789_H__
 #define __ST7789_H__
@@ -37,60 +35,39 @@
   // PORT/PIN definition
   // -----------------------------------
   #define PORT                  PORTB
-  #define DDR                   DDRB 
-  #define ST7789_MOSI           3 // SDA
+  #define DDR                   DDRB
+  #define ST7789_MOSI           3     // SDA
   #define ST7789_MISO           4
-  #define ST7789_SCK            5 // SCL
+  #define ST7789_SCK            5     // SCL
 
   // Command definition
   // -----------------------------------
-  #define DELAY                 0x80
-  
-  #define NOP                   0x00
-  #define SWRESET               0x01
-  #define RDDID                 0x04
-  #define RDDST                 0x09
+  #define NOP                   0x00  // This command is empty command.
+  #define SWRESET               0x01  // The display module performs a software reset, registers are written with their SW reset default values
 
-  #define SLPIN                 0x10
-  #define SLPOUT                0x11
-  #define PTLON                 0x12
-  #define NORON                 0x13
+  #define SLPIN                 0x10  // In this mode the DC/DC converter is stopped, internal oscillator is stopped, and panel scanning is stopped.
+  #define SLPOUT                0x11  // In this mode the DC/DC converter is enable, internal display oscillator is started, and panel scanning is started
+  #define PTLON                 0x12  // This command turns on Partial mode. The partial mode window is described by the Partial Area command (30h)
+  #define NORON                 0x13  // This command turns the display to normal mode, Normal display mode on means partial mode off
 
-  #define INVOFF                0x20
-  #define INVON                 0x21
-  #define DISPOFF               0x28
-  #define DISPON                0x29
-  #define RAMRD                 0x2E
-  #define CASET                 0x2A
-  #define RASET                 0x2B
-  #define RAMWR                 0x2C
+  #define INVOFF                0x20  // This command is used to recover from display inversion mode
+  #define INVON                 0x21  // This command is used to recover from display inversion mode.
+  #define DISPOFF               0x28  // In this mode, the output from Frame Memory is disabled and blank page inserted
+  #define DISPON                0x29  // This command is used to recover from DISPLAY OFF mode.
+  #define CASET                 0x2A  // Column Address Set, XS, XE [15:0] < 239 (00Efh)): MV=”0”, XS, XE [15:0] < 319 (013Fh)): MV=”1”
+  #define RASET                 0x2B  // Row Address Set, 0 < YS [15:0] < YE [15:0] < 319 (013fh)): MV=”0”, 0 < YS [15:0] < YE [15:0] < 239 (00EFh)): MV=”1”
+  #define RAMWR                 0x2C  // Memory Write, This command is used to transfer data from MCU to frame memory.
 
-  #define PTLAR                 0x30
-  #define MADCTL                0x36
-  #define COLMOD                0x3A
+  #define PTLAR                 0x30  // Partial Area
+  #define MADCTL                0x36  // Memory Data Access Control
+  #define COLMOD                0x3A  // Interface Pixel Format
+  #define TEOFF                 0x34  // Tearing Effect Line OFF
+  #define TEON                  0x35  // Tearing Effect Line On, This command is used to turn ON the Tearing Effect output signal from the TE signal line
+  #define VSCSAD                0x37  // Vertical Scroll Start Address of RAM
+  #define IDMOFF                0x38  // Idle Mode Off
+  #define IDMON                 0x39  // Idle Mode On
 
-  #define FRMCTR1               0xB1
-  #define FRMCTR2               0xB2
-  #define FRMCTR3               0xB3
-  #define INVCTR                0xB4
-  #define DISSET5               0xB6
-
-  #define PWCTR1                0xC0
-  #define PWCTR2                0xC1
-  #define PWCTR3                0xC2
-  #define PWCTR4                0xC3
-  #define PWCTR5                0xC4
-  #define VMCTR1                0xC5
-
-  #define RDID1                 0xDA
-  #define RDID2                 0xDB
-  #define RDID3                 0xDC
-  #define RDID4                 0xDD
-
-  #define GMCTRP1               0xE0
-  #define GMCTRN1               0xE1
-
-  #define PWCTR6                0xFC
+  #define WRCTRLD               0x53  // Write CTRL Display
 
   // Colors
   // -----------------------------------
@@ -100,8 +77,8 @@
 
   // AREA definition
   // -----------------------------------
-  #define MAX_X                 161                     // max columns / MV = 0 in MADCTL
-  #define MAX_Y                 130                     // max rows / MV = 0 in MADCTL
+  #define MAX_X                 280                     // max columns / MV = 0 in MADCTL
+  #define MAX_Y                 240                     // max rows / MV = 0 in MADCTL
   #define SIZE_X                MAX_X - 1               // columns max counter
   #define SIZE_Y                MAX_Y - 1               // rows max counter
   #define CACHE_SIZE_MEM        (MAX_X * MAX_Y)         // whole pixels
@@ -112,12 +89,12 @@
   // -----------------------------------
   #define CLR_BIT(port, bit)                            ( ((port) &= ~(1 << (bit))) )
   #define SET_BIT(port, bit)                            ( ((port) |= (1 << (bit))) )
-  #define IS_BIT_CLR(port, bit)                         ( IS_BIT_SET(port, bit) ? 0 : 1 )
   #define IS_BIT_SET(port, bit)                         ( ((port) & (1 << (bit))) ? 1 : 0 )
-  #define WAIT_UNTIL_BIT_IS_SET(port, bit)              { while (IS_BIT_CLR(port, bit)); }
+  #define IS_BIT_CLR(port, bit)                         ( IS_BIT_SET(port, bit) ? 0 : 1 )
+  #define WAIT_IF_BIT_SET(port, bit)                    { while (IS_BIT_CLR(port, bit)); }
 
-  extern const uint8_t INIT_ST7789[];                   // @const Command list ST7789B 
-  
+  extern const uint8_t INIT_ST7789[];                   // @const Command list ST7789B
+
   /** @enum Font sizes */
   enum Size {
     X1 = 0x00,                                          // 1x high & 1x wide size
@@ -169,7 +146,7 @@
   void ST7789_Send_Command (struct st7789 *, uint8_t);
 
   /**
-   * @desc    8bits data send
+   * @desc    Data send - byte
    *
    * @param   struct st7789 *
    * @param   uint8_t
